5b5fd91aca39d59f6d66f763d56148ab1df20349
Huaying Qiu <huayingq1996@gmail.com>
2022-07-13 09:09:22 -0400

    script to combine multiple tiffs into a pyramidal ome-tiff

---
+++ src/generate_ome_tiff.py
@@ -0,0 +1,97 @@
+import os 
+import sys
+import numpy as np
+import tifffile
+
+#####################################
+# Parse command line arguments
+#####################################
+
+opts = [opt for opt in sys.argv[1:] if opt.startswith('-')]
+
+if "-o" in opts:
+    ALTERNATIVE_OUTPUT = True
+else:
+    ALTERNATIVE_OUTPUT = False
+
+try: 
+    PATH = sys.argv[1]
+    RESOLUTION_X = sys.argv[2]
+    RESOLUTION_Y = sys.argv[3]
+except IndexError:
+    raise SystemExit(f"Please input the following arguments.\
+        \n Usage: {sys.argv[0]} <Path> <Resolution_X> <Resolution_Y> -o<Output name>\
+        \n <PATH> : Path to the directory with TIFF files you want to combine. \
+        \n <Resolution_X> : Pixel width in μm. \
+        \n <Resolution_Y> : Pixel height in μm. \
+        \n -o : Specifies an alternative output name. If omitted, the output will be named after the directory.")
+    
+
+#####################################
+# User inputs
+#####################################
+
+# pixel width in μm
+RESOLUTION_X = sys.argv[2]
+# pixel height in μm
+RESOLUTION_Y = sys.argv[3]
+
+# Set img directory
+
+img_dir = sys.argv[1]
+
+# Get tiff names in img_dir
+
+extensions = ['tif', 'tiff']
+
+img_names = [file for file in os.listdir(img_dir) if any(file.endswith(ext) for ext in extensions)]
+
+# remove .tiff for channel names 
+
+channel_names = [elem.split('.')[0] for elem in img_names]
+
+###############################################
+#  Load tiff files into array and stack them
+###############################################
+
+#images = [img_dir + '/' + img for img in img_names]
+
+imgList = list() # Create an empty list to store image arrays
+
+# Read tiff and append to the list
+for img in img_names:
+    img_path = img_dir + '/' + img
+    tiff = tifffile.imread(img_path)
+    imgList.append(tiff)
+
+# Stack arrays
+imgStack = np.stack(imgList)
+
+imgStack = imgStack.astype('uint16')
+
+#########################################################
+# Write pyramidal OME-TIFF
+#########################################################
+
+if ALTERNATIVE_OUTPUT:
+    output_name = opts[0].split('-o')[1]
+else: 
+    output_name = os.path.split(img_dir)[-1]
+
+tif_name = output_name + '.ome.tif'
+
+with tifffile.TiffWriter(tif_name, bigtiff=True) as tif:
+    options = dict(tile = (128, 128),
+                    metadata = {'Channel': {'Name': channel_names},
+                    'Pixels' : {
+                        'PhysicalSizeX': RESOLUTION_X,
+                        'PysicalSizeXUnit': 'µm',
+                        'PhysicalSizeY': RESOLUTION_Y,
+                        'PysicalSizeYUnit': 'µm'
+                    }
+                    })
+    tif.write(imgStack, subifds = 4, **options)
+    tif.write(imgStack[:, ::2, ::2], subfiletype = 1, **options)
+    tif.write(imgStack[:, ::4, ::4], subfiletype = 1, **options)
+    tif.write(imgStack[:, ::8, ::8], subfiletype = 1, **options)
+    tif.write(imgStack[:, ::16, ::16], subfiletype = 1, **options)
